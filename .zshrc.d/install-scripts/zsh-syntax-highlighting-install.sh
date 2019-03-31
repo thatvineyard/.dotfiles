ac8c0d705c99e38c0cdccd986be4e0d9983a0d61
Carl Wingårdh <c.a.wingardh@gmail.com>
2019-03-31 14:30:33 +0200

    Added an install script and better colors for ls

---
+++ .zshrc.d/install-scripts/zsh-syntax-highlighting-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# run by calling ". ./zsh-syntax-highlighting-install.sh" to include environment
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
